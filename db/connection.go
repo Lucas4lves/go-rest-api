1af0b67b8145ff42b2bffa44d32f37512f1d6a5b
lucas4lves <zombnfl@gmail.com>
2025-01-20 22:12:53 -0300

    Testing different ways to pass a db config

--- db/connection.go
+++ db/connection.go
@@ -4,37 +4,68 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	host     = "postgres-db"
-	port     = "5432"
-	user     = "postgres"
-	password = "12345"
-	dbbame   = "postgres"
-)
+type DbConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Name     string
+}
 
-func ConnectDb() (*sql.DB, error) {
+func CreateDbConfig(host string, port string, user string, password string, name string) DbConfig {
+	return DbConfig{
+		Host:     host,
+		Port:     port,
+		User:     user,
+		Password: password,
+		Name:     name,
+	}
+}
+
+type Db struct {
+	Instance *sql.DB
+	Config   DbConfig
+}
+
+func NewDb(sql *sql.DB, host string, port string, user string, password string, name string) (*Db, error) {
+	return &Db{
+		Instance: sql,
+		Config:   CreateDbConfig(os.Getenv("HOST"), os.Getenv("PORT"), os.Getenv("USER"), os.Getenv("PASS"), os.Getenv("NAME")),
+	}, nil
+}
+
+// const (
+// 	host     = "postgres-db"
+// 	port     = "5432"
+// 	user     = "testuser"
+// 	password = "1234"
+// 	dbbame   = "postgres"
+// )
+
+func (db *Db) ConnectDb() (*sql.DB, error) {
 	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
-		"password=%s dbname=%s sslmode=disable", host, port, user, password, dbbame)
+		"password=%s dbname=%s sslmode=disable", db.Config.Host, db.Config.Port, db.Config.User, db.Config.Password, db.Config.Name)
 
 	fmt.Println(psqlInfo)
 
-	db, err := sql.Open("postgres", psqlInfo)
+	database, err := sql.Open("postgres", psqlInfo)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	err = db.Ping()
+	err = database.Ping()
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println("Connected to " + dbbame)
+	fmt.Println("Connected to " + db.Config.Name)
 
-	return db, nil
+	return database, nil
 }
